131a0d09ac8c0ca8dd7be63f28f2c4426f556c1b
Oskarrro03 <oskarrochowiak@gmail.com>
2022-12-09 14:46:04 +0100

    commit

--- index.js
+++ index.js
@@ -1,148 +1,100 @@
+Object.byString = (o, s) => {
+  s = s.replace(/\[(\w+)\]/g, ".$1");
+  s = s.replace(/^\./, "");
+  let a = s.split(".");
+  for (let i = 0, n = a.length; i < n; ++i) {
+    let k = a[i];
+    if (k in o) {
+      o = o[k];
+    } else {
+      return;
+    }
+  }
+  return o;
+};
+
+let GenerateAnswer = () => {
+  let array = [];
+  for (let i = 0; i < 4; i++) {
+    array.push(
+      Object.byString(Colors, Math.floor(Math.random() * 4).toString())
+    );
+  }
+
+  return array;
+};
+
 //variables
-let colorArrM = [];
-let colorArrP = [];
-let turn = "mastermind";
-let colorCheck = [];
-let colorAutoCheck = [];
 let round = 0;
+
+let Colors = {
+  0: "red",
+  1: "navy",
+  2: "green",
+  3: "blue",
+  4: "yellow",
+  5: "pink",
+};
+
+let GameHistory = {};
+
+let GameState = {
+  answer: GenerateAnswer(),
+  player: [],
+  check: [],
+};
 
 //choose colors on button clicks
 $("#red,#green,#blue,#navy,#pink,#yellow").on("click", function () {
-  //mastermind turn
-  if (turn === "mastermind") {
-    colorArrM.length < 4
-      ? colorArrM.push($(this).attr("id"))
-      : console.log("4 is max");
-    console.log(colorArrM);
-    //player turn
-  } else if (turn === "player") {
-    colorArrP.length < 4
-      ? colorArrP.push($(this).attr("id"))
-      : console.log("4 is max");
-    console.log(colorArrP);
-  }
+  GameState.player.length < 4
+    ? GameState.player.push($(this).attr("id"))
+    : console.log("4 is max");
+  console.log(GameState.player);
 });
 
-//give control to other player
 $("#accept").on("click", () => {
-  //mastermind turn
-  if (turn === "mastermind") {
-    if (colorArrM.length === 4) {
-      console.log("player turn");
-      turn = "player";
-    } else {
-      colorArrM.length === 3
-        ? console.log(`You still have to choose 1 more color!`)
-        : console.log(
-            `You still have to choose ${4 - colorArrM.length} more colors!`
-          );
-    }
-    //player turn
-  } else if (turn === "player") {
-    if (colorArrP.length === 4) {
-      console.log("mastermind turn");
-      turn = "mCheck";
-      $("#buttonWrapper").css({ bottom: "-1000px" });
-      $("#blackWhiteWrapper").css({ bottom: "5%" });
-      colorFinder();
-    } else {
-      colorArrP.length === 3
-        ? console.log(`You still have to choose 1 more color!`)
-        : console.log(
-            `You still have to choose ${4 - colorArrM.length} more colors!`
-          );
-    }
-    //check turn
-  } else if (turn === "mCheck") {
-    if (colorAutoCheck.length === colorCheck.length) {
-      checkIfEqual();
-    } else {
-      colorCheck = [];
-      console.log("thats not true");
-    }
+  if (GameState.player.length === 4) {
+    colorFinder();
+  } else {
+    GameState.player.length === 3
+      ? console.log(`You still have to choose 1 more color!`)
+      : console.log(
+          `You still have to choose ${4 - GameState.player.length} more colors!`
+        );
   }
 });
 
 //remove last color from array
 $("#remove").on("click", () => {
-  //mastermind turn
-  if (turn === "mastermind") {
-    colorArrM.pop();
-    console.log(colorArrM);
-    //player turn
-  } else if (turn === "player") {
-    colorArrP.pop();
-    console.log(colorArrP);
-  } else if (turn === "mCheck") {
-    colorCheck.pop();
-    console.log(colorCheck);
-  }
-});
-
-//black and white buttons
-$("#black, #white").on("click", function () {
-  if (colorCheck.length < 4) {
-    colorCheck.push($(this).attr("id"));
-    console.log(colorCheck);
-  } else {
-    return;
-  }
+  GameState.player.pop();
+  console.log(GameState.player);
 });
 
 function colorFinder() {
   //place white pins
   for (let i = 0; i < 4; i++) {
-    if (colorArrM[i] === colorArrP[i]) {
-      colorAutoCheck.push("white");
-      colorArrP[i] = "replaced";
+    if (GameState.answer[i] === GameState.player[i]) {
+      GameState.check.push("white");
+      GameState.player[i] = "replaced";
     }
   }
   //place black pins
   for (let i = 0; i < 4; i++) {
-    if (colorArrP.includes(colorArrM[i]) === true) {
-      colorAutoCheck.push("black");
-      colorArrP.remove(colorArrM[i]);
+    if (GameState.player.includes(GameState.answer[i]) === true) {
+      GameState.check.push("black");
+      GameState.player.remove(GameState.answer[i]);
     }
   }
-}
-
-//new round function
-function startNewRound() {
-  if (round === 9) {
-    console.log("mastermind won the game");
-    startNewGame();
-  } else {
-    console.log("new round");
-    colorArrM = [];
-    colorArrP = [];
-    turn = "mastermind";
-    colorCheck = [];
-    colorAutoCheck = [];
-    round++;
-    $("#buttonWrapper").css({ bottom: "2%" });
-    $("#blackWhiteWrapper").css({ bottom: "-1000px" });
-  }
-}
-
-//new game function
-function startNewGame() {
-  console.log("new game");
-  colorArrM = [];
-  colorArrP = [];
-  turn = "mastermind";
-  colorCheck = [];
-  colorAutoCheck = [];
-  round = 0;
-  $("#buttonWrapper").css({ bottom: "2%" });
-  $("#blackWhiteWrapper").css({ bottom: "-1000px" });
+  console.log(GameState.check);
+  checkWinCondition();
 }
 
 //win condition checking
 function checkWinCondition() {
   let i;
   for (i = 0; i < 4; i++) {
-    if (colorAutoCheck.includes("white") === true) {
-      colorAutoCheck.remove("white");
+    if (GameState.check.includes("white") === true) {
+      GameState.check.remove("white");
     } else {
       startNewRound();
       return;
@@ -155,24 +107,26 @@ function checkWinCondition() {
   }
 }
 
-//checking if colorAutoCheck === colorCheck
-function checkIfEqual() {
-  let i;
-  for (i = 0; i < colorAutoCheck.length; i++) {
-    if (colorCheck.includes(colorAutoCheck[i]) === true) {
-      colorCheck.remove(colorAutoCheck[i]);
-    } else {
-      colorCheck = [];
-      console.log("thats wrong");
-      return;
-    }
-  }
-  if (i === colorAutoCheck.length) {
-    checkWinCondition();
+//new round function
+function startNewRound() {
+  if (round === 9) {
+    console.log("mastermind won the game");
+    startNewGame();
   } else {
-    colorCheck = [];
-    console.log("thats wrong");
+    console.log("new round");
+    GameState.player = [];
+    GameState.check = [];
+    round++;
   }
+}
+
+//new game function
+function startNewGame() {
+  console.log("new game");
+  GameState.answer = GenerateAnswer();
+  GameState.player = [];
+  GameState.check = [];
+  round = 0;
 }
 
 //array.remove() function
